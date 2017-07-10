d7b70ba486bdf58617e38f0d551cea48988373c9
reavess <reavess7799@gmail.com>
2017-07-09 22:44:46 -0600

    php

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+function connect(){
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:hatorders.database.windows.net,1433; Database = Orders", "sean", "{shushk!7}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "sean@hatorders", "pwd" => "{shushk!7}", "Database" => "Orders", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:hatorders.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+}
+?>
