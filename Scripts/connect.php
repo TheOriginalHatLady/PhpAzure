0377f5cee3df4ba821a6a978409dfe692fc3442b
reavess <reavess7799@gmail.com>
2017-07-11 23:05:50 -0600

    all

--- Scripts/connect.php
+++ Scripts/connect.php
@@ -19,7 +19,7 @@ $connectionInfo = array("UID" => "sean@hatorders", "pwd" => "{shushk!7}", "Datab
 $serverName = "tcp:hatorders.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-/*
+
 $statement = $conn->prepare("INSERT INTO dbo.Details(Id, ProductType, Colors, Description, Size)
     VALUES(?, ?, ?, ?, ?)");
     
@@ -28,5 +28,5 @@ $statement->execute(array("1", "Hat", "Gray", "Super cool", "medium"));
 
 echo "succes";
 echo "chuckle";
-*/
+
 ?>/
